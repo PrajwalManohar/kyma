7f98ba4827cebf6736b0107221f78fe5250dacaa
PrajwalManohar <mprajwal1998@gmail.com>
2023-11-13 21:11:51 +0530

    testing CICD job

---
+++ bookstore/temp.ps1
@@ -0,0 +1,33 @@
+$ns = "tutorial"
+$API_SERVER_URL = kubectl config view -o=jsonpath='{.clusters[].cluster.server}'
+
+$SECRET_NAME = "tutorial-service-account"
+
+$CA = kubectl get secret/$SECRET_NAME -n $ns -o jsonpath='{.data.ca\.crt}'
+
+$TOKEN = kubectl get secret/$SECRET_NAME -n $ns -o jsonpath='{.data.token}'
+$DEC_TOKEN = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($TOKEN))
+
+Add-Content -Path kubeconfig.yaml @"
+apiVersion: v1
+kind: Config
+clusters:
+- name: default-cluster
+  cluster:
+    certificate-authority-data: $CA
+    server: $API_SERVER_URL
+users:
+- name: default-user
+  user:
+    token: $DEC_TOKEN
+contexts:
+- name: default-context
+  context:
+    cluster: default-cluster
+    namespace: $ns
+    user: default-user
+current-context: default-context
+"@
+
+
+Write-Host "Finished"
